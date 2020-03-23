5a47a41a5b0719939818c74b1ee4cbf390f09ed0
Hannnes1 <hannes.hultergard@gmail.com>
2020-03-23 18:02:37 +0100

    Klar med ex6 i week1

--- week1/exercises/ex6fiddling.c
+++ week1/exercises/ex6fiddling.c
@@ -31,11 +31,11 @@ int get_digit_at_index(int number, int index);
 // Swap two digits in a number using indices like above
 int swap(int number, int from_index, int to_index);
 
+int pow_int(int base, int exp);
+
 
 int main(void) {
 
-    // TODO Uncomment one at a time, all should print true
-    /*
     EQUALS(n_digits(123), 3);   // 123 has 3 digits
     EQUALS(n_digits(12345), 5);
     EQUALS(n_digits(1023945), 7);
@@ -51,14 +51,57 @@ int main(void) {
     EQUALS(swap(12345, 0, 4), 52341);
     EQUALS(swap(12345, 4, 4), 12345);
     EQUALS(swap(12345, 3, 3), 12345);
-    */
+
     return 0;
 }
 
 // ----------------- Function definitions ---------------------
 
 int n_digits(int number) {
-   // TODO 
+    int sum = 0;
+    while (number > 0) {
+        sum++;
+        number /= 10;
+    }
+    return sum;
 }
 
-// TODO Add more function definitions
+bool digit_is_in(int number, int digit) {
+    bool res = false;
+    while (number > 0) {
+        if (number % 10 == digit) {
+            res = true;
+        }
+        number /= 10;
+    }
+    return res;
+}
+
+int get_digit_at_index(int number, int index) {
+    int currentIndex = n_digits(number);
+    int digit = 0;
+    while (number > 0) {
+        if (index == --currentIndex) {
+            digit = number % 10;
+        }
+        number /= 10;
+    }
+    return digit;
+}
+
+int swap(int number, int from_index, int to_index) {
+    int num1 = get_digit_at_index(number, from_index);
+    int num1Multiplier = pow_int(10, n_digits(number) - from_index - 1);
+    int num2 = get_digit_at_index(number, to_index);
+    int num2Multiplier = pow_int(10, n_digits(number) - to_index - 1);
+
+    return (number - num1 * num1Multiplier - num2 * num2Multiplier + num1 * num2Multiplier + num2 * num1Multiplier);
+}
+
+int pow_int(int base, int exp) {
+    int res = 1;
+    for (int i = 0; i < exp; ++i) {
+        res *= base;
+    }
+    return res;
+}
