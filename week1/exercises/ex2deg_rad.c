8374b730d51b27ca9f64bbc87465ea12e4021a1a
Hannnes1 <hannes.hultergard@gmail.com>
2020-03-23 15:53:47 +0100

    Klar med ex2 i week1

--- week1/exercises/ex2deg_rad.c
+++ week1/exercises/ex2deg_rad.c
@@ -16,20 +16,37 @@ int main(void) {
 
     // ---- Input ---------
     printf("Select\n1) For Deg to rad\n2) For Rad to deg\n> ");
-    scanf("%d", &selection);
-    
-    // TODO 
+    scanf_s("%d", &selection);
+
+    if (selection == 1) {
+        printf("Input angle in deg >");
+        scanf_s("%f", &input);
+    } else if (selection == 2) {
+        printf("Input angle in rad >");
+        scanf_s("%f", &input);
+    } else {
+        printf("ERROR: Invalid input!");
+        return 1; //Stop the program if the input is invalid
+    }
 
     // ------ Process --------
-    double result;  
-   
-    // TODO 
+    double result;
+
+    if(selection == 1){
+        result = input*PI/180;
+    } else{
+        result = input*180/PI;
+    }
 
     // ----- Output ------------
-    
-    // TODO 
-    
-   
+
+    if(selection == 1){
+        printf("%.4f deg = %.4f rad", input, result);
+    } else{
+        printf("%.4f rad = %.4f deg", input, result);
+    }
+
+
     return 0;
 }
 
